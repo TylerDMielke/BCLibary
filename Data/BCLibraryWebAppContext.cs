fdad4d257ff4989ce9e81d94219240cd3c86a948
Ler Daddy <mielkedtyler@gmail.com>
2021-07-02 21:19:56 -0500

    Update dependencies and scaffold Books model

---
+++ Data/BCLibraryWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BCLibraryWebApp.Models;
+
+namespace BCLibraryWebApp.Data
+{
+    public class BCLibraryWebAppContext : DbContext
+    {
+        public BCLibraryWebAppContext (DbContextOptions<BCLibraryWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BCLibraryWebApp.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BCLibraryWebApp.Data;
+using BCLibraryWebApp.Models;
+
+namespace BCLibraryWebApp.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BCLibraryWebApp.Data.BCLibraryWebAppContext _context;
+
+        public DetailsModel(BCLibraryWebApp.Data.BCLibraryWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
