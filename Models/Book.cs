9016f3e99993437ffccc0b585bb75934fc1d167c
Ler Daddy <mielkedtyler@gmail.com>
2021-07-02 21:14:11 -0500

    Add models folder and Book model

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BCLibraryWebApp.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
